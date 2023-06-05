3b8fe0f6c213da922eefe5ddefe36f05b7a08a12
EvgenyShalimo <zhenya_shalimo@mail.ru>
2023-06-05 16:55:37 +0300

    Добовляем Динамически изменяемые страницы для команты с товаром

--- store/main/urls.py
+++ store/main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/',include('users.urls')),
     path('',include('home.urls')),
+    path('products/',include('storage.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- store/storage/views.py
+++ store/storage/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Product
 
-# Create your views here.
+
+def products_list(request):
+    return render(request, 'storage/products_list.html')
+
+
+class PruductView(DetailView):
+    model = Product
+    template_name = 'storage/product_room.html'
+    context_object_name = 'product'
