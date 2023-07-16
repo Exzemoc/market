60fc2551fb584df301c272b7390b042fc0980e4c
PC <knohinov_vlad@mail.ru>
2023-07-16 16:35:39 +0300

    fix + courierfix + css

--- store/staff_status/urls.py
+++ store/staff_status/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('add_product/', views.create_product, name='add_product'),
     path('delete_product/', views.delete_products_list, name='delete_product_list'),
     path('delete_product/<int:product_id>/', views.delete_product, name='delete_product'),
-
+    path('accept_delivery/<int:order_id>/', views.accept_delivery, name='accept_delivery'),
+    path('close_delivery/<int:order_id>/', views.close_delivery, name='close_delivery'),
 ]
